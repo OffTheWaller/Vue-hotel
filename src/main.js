f2a41941dc87ec6da8ab7b4d59a350ae84d7189b
OffTheWaller <1031118178@qq.com>
2018-12-05 20:52:04 +0800

    vue/cli3.x 初始化模板

--- src/main.js
+++ src/main.js
@@ -1,29 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-// 初始化css
-import './assets/styles/reset.css'
-// 1px像素问题
-import './assets/styles/border.css'
-//移动端点击延迟问题
-import fastClick from 'fastclick'
-//引入iconfont
-import './assets/styles/iconfont.css'
-//引入swiper
-import VueAwesomeSwiper from 'vue-awesome-swiper'
-import 'swiper/dist/css/swiper.css'
-Vue.use(VueAwesomeSwiper)
-
-fastClick.attach(document.body)
+import store from './store'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  render: h => h(App)
+}).$mount('#app')
